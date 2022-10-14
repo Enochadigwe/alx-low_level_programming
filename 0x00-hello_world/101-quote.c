9c3fe5f1691ecb97e010cd5208fff043399015a9
Enoch David Adigwe <enochadigwe@gmail.com>
2022-10-14 16:11:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *  *main-program entry point.
- *   *
- *    *Return: 1 on success.
- *     */
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
