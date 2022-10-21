acca9225ff7fc82e4132fcf47fa17c6a0afcbfd6
Enoch David Adigwe <enochadigwe@gmail.com>
2022-10-21 00:57:52 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,33 @@
-#include "main.h"
-void print_integer(int m);
-/**
- * * print_number - a function that prints an integer.
- * * @n: An input integer
- * * Return: Nothing
-*/
-void print_number(int n)
-{
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
-	{
-		_putchar('-');
-		print_integer(n * -1);
-	}
-	else
-		print_integer(n);
-}
-/**
- * * print_integer - A function to priting n
- * * @m: an input unsigned integer
- * * Return: Nothing
-*/
-void print_integer(int m)
-{
-	int i = 1000000000;
+#include <stdio.h>
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+/**
+ * main-entry point
+ *
+ * Description: prints all possible combinations of two two-digit numbers
+ *
+ * Return:0 success, non zero fail
+ */
+
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 99; i++)
+{
+putchar('0' + i / 10);
+putchar('0' + i / 10);
+
+putchar(' ')
+
+putchar('0' + j / 10);
+putchar('0' + j / 10);
+
+if (i == 98 && j == 99)
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
