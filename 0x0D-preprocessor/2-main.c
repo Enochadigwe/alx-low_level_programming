4a01c7eab672e69284e1ce11847d407708faf60a
Enoch David Adigwe <enochadigwe@gmail.com>
2022-11-15 11:19:41 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>
 
-/*
-** main - prints the name of the file.
-**
-** Return: Always 0.
-*/
-int main(void)
+/**
+ ** main - prints the name of the file.
+ **
+ ** Return: Always 0.
+ **/
+int main(void)
 {
-	printf("%s\n",  __FILE__);
-        return (0);
+		printf("%s\n", __FILE__);
+			return (0);
 }
